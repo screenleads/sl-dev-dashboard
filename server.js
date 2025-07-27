7ece883246d3c4eed4d4fdaf42123d206f2bb6b9
jatod <jatod@segurcaixaadeslas.es>
2025-07-27 12:29:27 +0200

    force update with new angular.json

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Servidor Express funcionando ✅');
+// ✅ Corregido: ruta sin 'browser'
+const distFolder = path.join(__dirname, 'dist', 'sl-dev-deshboard');
+
+app.use(express.static(distFolder));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distFolder, 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en puerto ${port}`);
+    console.log(`Servidor Angular escuchando en puerto ${port}`);
 });
