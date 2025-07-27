ac1318fd6338d1e223142e779f18631d32ddc478
jatod <jatod@segurcaixaadeslas.es>
2025-07-27 12:18:55 +0200

    force to test

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// ✅ Corregido: ruta sin 'browser'
-const distFolder = path.join(__dirname, 'dist', 'sl-dev-deshboard');
-
-app.use(express.static(distFolder));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(distFolder, 'index.html'));
+app.get('/', (req, res) => {
+    res.send('Servidor Express funcionando ✅');
 });
 
 app.listen(port, () => {
-    console.log(`Servidor Angular escuchando en puerto ${port}`);
+    console.log(`Servidor escuchando en puerto ${port}`);
 });
